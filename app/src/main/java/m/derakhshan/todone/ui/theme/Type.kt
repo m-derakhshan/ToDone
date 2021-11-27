3a47a63a5ea8754d3d9828b9b3cfaf37bc3ec3c2
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2021-11-27 19:41:15 +0330

    adding font family

--- app/src/main/java/m/derakhshan/todone/ui/theme/Type.kt
+++ app/src/main/java/m/derakhshan/todone/ui/theme/Type.kt
@@ -2,17 +2,27 @@ package m.derakhshan.todone.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import m.derakhshan.todone.R
+
+val myFont = FontFamily(Font(R.font.font, weight = FontWeight.Normal))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = myFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    button = TextStyle(
+        fontFamily = myFont,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    defaultFontFamily = myFont
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
