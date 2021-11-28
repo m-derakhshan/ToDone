43da64677983ef73d012c3d4d3d99f15e9dc4b62
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2021-11-28 18:36:32 +0330

    adding other screens

---
+++ app/src/main/java/m/derakhshan/todone/feature_tasks/presentation/TasksViewModel.kt
@@ -0,0 +1,10 @@
+package m.derakhshan.todone.feature_tasks.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TasksViewModel @Inject constructor() : ViewModel() {
+
+}
